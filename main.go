d291d19ca5f5cea32bce6cc6e14ae199023163b9
pavly-remon <pavlyremon@gmail.com>
2025-02-06 15:47:51 +0200

    no-join-point-issue

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+/*
+According to fork-join model:
+the anonymous function will be forked but there is no join point
+So, it is undetermined if sayHello function will be executed before main goroutine exits
+*/
 func main() {
-	fmt.Println("Concurrency in Go")
+	sayHello := func() {
+		fmt.Println("Concurrency in Go")
+	}
+	go sayHello()
+	fmt.Println("Hello World")
 }
