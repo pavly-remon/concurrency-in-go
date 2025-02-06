7c8df4d921696134879f92cee3d0d0fdadd3b5d8
pavly-remon <pavlyremon@gmail.com>
2025-02-06 15:51:42 +0200

    Solution

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+/* Go FORK-JOIN Model */
 
 /*
-According to fork-join model:
 the anonymous function will be forked but there is no join point
 So, it is undetermined if sayHello function will be executed before main goroutine exits
 */
+
+/*
+can be solved using sync package to create join point
+*/
 func main() {
+	var wg sync.WaitGroup
 	sayHello := func() {
+		defer wg.Done()
 		fmt.Println("Concurrency in Go")
 	}
+	wg.Add(1)
 	go sayHello()
 	fmt.Println("Hello World")
+	wg.Wait()
 }
