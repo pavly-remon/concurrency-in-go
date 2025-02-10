fc9c7ab5d2fdae526ed32066cb0d1f9eccd90fde
pavly-remon <pavlyremon@gmail.com>
2025-02-10 11:16:09 +0200

    sync-package-mutex

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-	fmt.Println("Concurrency in Go")
+	count := 0
+	var wg sync.WaitGroup
+	var lock sync.Mutex
+	increment := func() {
+		defer wg.Done()
+		lock.Lock()
+		defer lock.Unlock()
+		count++
+		fmt.Printf("Increment Count: %d\n", count)
+
+	}
+	decrement := func() {
+		defer wg.Done()
+		lock.Lock()
+		defer lock.Unlock()
+		count--
+		fmt.Printf("Decrement Count: %d\n", count)
+
+	}
+	for i := 0; i < 10; i++ {
+		wg.Add(2)
+		go increment()
+		go decrement()
+	}
+	wg.Wait()
 }
