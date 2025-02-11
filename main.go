f191eef56beb501cb0a940056fd112d0a00c96f8
pavly-remon <pavlyremon@gmail.com>
2025-02-11 10:57:45 +0200

    sync-package-cond

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 func main() {
-	fmt.Println("Concurrency in Go")
+	cond := sync.NewCond(&sync.Mutex{})
+	queue := make([]interface{}, 0, 10)
+
+	removeFromQueue := func(delay time.Duration) {
+		time.Sleep(delay)
+		cond.L.Lock()
+		defer cond.L.Unlock()
+		defer cond.Signal()
+		queue = queue[1:]
+		fmt.Println("Removed from queue")
+	}
+
+	for i := 0; i < 10; i++ {
+		cond.L.Lock()
+		for len(queue) == 2 {
+			cond.Wait()
+		}
+		fmt.Println("Adding to Queue...")
+		queue = append(queue, struct{}{})
+		go removeFromQueue(1 * time.Second)
+		cond.L.Unlock()
+	}
 }
