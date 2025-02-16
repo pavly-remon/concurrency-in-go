021144f380afe67d206d7bfa1bf14cd77e875d45
pavly-remon <pavlyremon@gmail.com>
2025-02-16 11:37:38 +0200

    buffered-channels

--- main.go
+++ main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+func chanOwner() <-chan int {
+	c := make(chan int, 3)
+	go func() {
+		defer close(c)
+		for i := 0; i < 10; i++ {
+			c <- i
+		}
+	}()
+	return c
+}
 func main() {
-	fmt.Println("Concurrency in Go")
+	outputStream := chanOwner()
+	for v := range outputStream {
+		fmt.Printf("Received: %d\n", v)
+	}
+	fmt.Println("Done Receiving")
 }
